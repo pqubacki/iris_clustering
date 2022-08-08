70ea5aae94fe2298003b2fcb3e162a0e7a03bb03
Piotr Kubacki <pqubacki@gmail.com>
2022-08-08 22:19:28 +0100

    final project

--- clustering.R
+++ clustering.R
@@ -25,28 +25,23 @@ library(cluster)
 library(ggplot2)
 
 # Importing the Iris dataset:
-df <- read.csv("C:\\Users\\Piotr Kubacki\\Documents\\CollegeDataScience\\Data science and analytics\\Project1\\DATA8001_Iris.csv")
+df <- read.csv("iris.csv")
 head(df)
 
-#change the name of the first column from  ?.?sepal_length to sepal_length
 names(df)[1] <- "sepal_length"
 head(df)
 
-# (a) Create a Scatter Matrix (Pairplot) of the Dataset [5 Marks]
+# Creating a Scatter Matrix (Pairplot) of the Dataset
 # Where each of the 4 variables are plotted against one another
 scatter.matrix<- pairs(df)
-
-# (b) Scale the Data [5 Marks]
-# Use your scaling method of choice, and either scale manually or using a built in function
 
 #scaling data set with built in function
 df <- scale(df)
 head(df)
 pairs(df)
 
-# (c) Use the elbow method to find the optimal # of clusters [5 Marks]
+# Using the elbow method to find the optimal number of clusters
 # The elbow method compare the Within Cluster Sum of Squares to the value of K
-
 #setting seed for random generator and initialization of empty vector to store Within clusters sum of squares from loop 
 set.seed(5)
 wcss <- vector()
@@ -67,10 +62,7 @@ plot(1:10, wcss, type = 'b', main = paste('Elbow method for clusters of Iris'), 
 
 
 
-# (d) Fit K-Means to the Dataset with the best value for K and predict the clusters of the dataset [5 Marks]
-#     - The best value for K can be established from above - 3
-#     - You can use the kmeans$cluster method to find the members
-#     - Add the Cluster membership to the Dataframe
+# Fit K-Means to the Dataset with the best value for K and predict the clusters of the dataset
 
 set.seed(23)
 kmeans <- kmeans(df, 3, iter.max = 300, nstart = 10)
@@ -86,8 +78,7 @@ df <- data.frame(df)
 
 View(df)
 
-# (e) Visualize the Cluster Membership [5 Marks]
-# Create at least one scatter plot with two different variables with the cluster membership set as the colour
+# Visualization of  Cluster Membership
 
 clusplot(df[,1:4],
          kmeans$cluster,
@@ -137,7 +128,7 @@ ggplot(df)+
 ###########################
 
 
-# (f) Create and plot the dendrogram for the Scaled Data [5 Marks]
+#dendrogram for the Scaled Data
 dendrogram <- hclust(dist(df[,-5], method = 'euclidean'), method = 'ward.D')
 plot(dendrogram,
      main = paste('Dendrogram'),
@@ -149,13 +140,6 @@ abline(h=100)
 
 #K = 3
 abline(h=30)
-
-
-
-# (g) Choose a number of clusters (cutoff), fit the Agglomerative Hierarchical Clustering with that number and predict the clusters of the dataset [5 Marks]
-#     - The optimal number of clusters can be established from the dendrogram above
-#     - You can use the cutree() method to predict the members
-#     - Add the Cluster membership to the Dataframe
 
 
 
@@ -174,7 +158,7 @@ View(df)
 
 
 
-# (h) Visualize the Cluster Membership [5 Marks]
+# Visualization of Cluster Membership
 # Create at least one scatter plot with two different variables with the cluster membership set as the color
 clusplot(df[,1:4],
          K2_hierarchical_predicted_cluster,
@@ -245,7 +229,7 @@ ggplot(df)+
 ########################################################################################################
 
 
-# (i) Do you find there to be agreement between the two solutions?
+# Conclusions about agreement between the two solutions?
 
 par(mfrow=c(1,3))
 
@@ -301,15 +285,9 @@ clusplot(df[,1:4],
 table(as.numeric(df$kmeans_predicted_cluster) == as.numeric(df$K3_hierarchical_predicted_cluster))
 
 
-
-
-# Is there an approach that you think does better? 
-#I think K-means approach is more precise as we do some precalculation defining number of clusters for this method
+#I think K-means approach is more precise as we do some pre calculation defining number of clusters for this method
 #I forced subjectively K = 3 for hclustering but dendrogram suggested rather 2 clusters
-
-# How many clusters do you believe are really in the data and why?
-# Again I believe there are 3 clusters observing behaviour of WCSS in elbow method and knowing history of dataset
+# I believe there are 3 clusters observing behaviour of WCSS in elbow method and knowing history of dataset
 # Although visually at first time you can clearly distinguish 2 and the difference between second and third
 # cluster is not that obvious
-# (comment just a couple of brief bullet points) [10 Marks]
 
